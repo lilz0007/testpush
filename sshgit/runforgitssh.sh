6273a721234ab791dc25277f79e5d16ab2864300
lilz0007 <bilalghassan77@gmail.com>
2022-08-19 14:00:43 +0300

    Update runforgitssh.sh

--- sshgit/runforgitssh.sh
+++ sshgit/runforgitssh.sh
@@ -1,3 +1,15 @@
-eval SSH_AUTH_SOCK=/tmp/ssh-XXXXXXlPQqsy/agent.33920; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=33921; export SSH_AGENT_PID;
-echo Agent pid 33921;
+ssh-agent -s
+ssh-add connection 
+##enter the password 1234 for the key provided 
+
+## eval $(ssh-agent -s)
+## ssh-add connection
+## more connection.pub 
+## ssh -T git@github.com
+## git add .
+## git commit
+## git config user.email bilalghassan77@gmail.com
+## git config user.name bilalpythonanywhere
+## git commit
+## git commit -m "from python anywhere to git"
+## git push git@github.com:lilz0007/Django-applicationsupport.git
